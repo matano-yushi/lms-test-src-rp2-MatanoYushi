617373cb7b812adfd5b661a95e30cf365558adf2
y-matano <y-matano@ISI-OA25L15>
2026-09-18 15:29:50 +0900

    fix：Case03　コース詳細画面テストコード追記修正
    コース詳細画面に遷移したことの確認のため

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -54,6 +54,7 @@ public class Case03 {
 		webDriver.findElement(By.name("password")).sendKeys("Pomupomupurin416");
 
 		webDriver.findElement(By.className("btn-primary")).click();
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
 
 		getEvidence(new Object() {
 		}, "");
