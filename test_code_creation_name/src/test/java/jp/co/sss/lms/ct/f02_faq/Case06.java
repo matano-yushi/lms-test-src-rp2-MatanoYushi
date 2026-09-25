a7a46f9f539d6572c417ad789111d4f808a268a0
y-matano <y-matano@ISI-OA25L15>
2026-09-25 09:06:07 +0900

    fix：Case06_test01～04追記修正
    トップページURLでアクセス、初回ログイン済みの受講生ユーザーでログイン、ヘルプ」リンクからヘルプ画面に遷移、「よくある質問」リンクからよくある質問画面を別タブに開く。テスト実施のため

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -18,6 +20,14 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース06 カテゴリ検索 正常系")
 public class Case06 {
+
+	private String detail = "http://localhost:8080/lms/course/detail";
+
+	private String login = "http://localhost:8080/lms/";
+
+	private String faq = "http://localhost:8080/lms/faq";
+
+	private String help = "http://localhost:8080/lms/help";
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,35 +45,66 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo(login);
+		assertEquals(login, webDriver.getCurrentUrl());
+		getEvidence(new Object() {
+		}, "");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).sendKeys("Pomupomupurin416");
+
+		webDriver.findElement(By.className("btn-primary")).click();
+		visibilityTimeout(By.cssSelector("button.navbar-btn"), 5);
+		assertEquals(detail, webDriver.getCurrentUrl());
+
+		getEvidence(new Object() {
+		}, "");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.className("dropdown-toggle")).click();
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+		assertEquals(help, webDriver.getCurrentUrl());
+
+		getEvidence(new Object() {
+		}, "");
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+
+		String originalWindowString = webDriver.getWindowHandle();
+		webDriver.findElement(By.linkText("よくある質問")).click();
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!originalWindowString.contentEquals(windowHandle)) {
+
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+		assertEquals(faq, webDriver.getCurrentUrl());
+
+		getEvidence(new Object() {
+		}, "");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+
 	}
 
 	@Test
